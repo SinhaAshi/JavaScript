8594f019e0bc3bcac194a71843e9cfa49c9ae6d2
SinhaAshi <166291323+SinhaAshi@users.noreply.github.com>
2025-07-26 10:19:07 +0000

    Learnt about dataTypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,9 @@
+"use strict"; //no need to write this now-a-days but it is used to treat all JS code as newer version
+// alert(3+3) //if we used browser or integrated js in a html file then a pop up would have shown 6 but it doesnt happen in node
+let name = "Ashi"
+let age = 21
+let isPresent = true
+let state;
+let city = null;
+console.log(typeof undefined);
+console.log(typeof name);
